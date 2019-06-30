c633ea4fc53417136e9be26195cf62e5e8b06703
joe <ebrightster@gmail.com>
2019-06-30 16:33:48 -0700

    initial version

---
+++ bin/main.dart
@@ -0,0 +1,5 @@
+import 'package:dart_json1/dart_json1.dart' as dart_json1;
+
+main(List<String> arguments) {
+  print('Hello world: ${dart_json1.calculate()}!');
+}
---
+++ test/dart_json1_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_json1/dart_json1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
